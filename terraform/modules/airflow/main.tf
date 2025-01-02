b0e1a684b16cb742eb2b487013c3a40ff4804216
Roman Bauer <roman.bauer@nabu.ee>
2025-01-02 18:06:49 +0200

    get rid of template_file provider

--- terraform/modules/airflow/main.tf
+++ terraform/modules/airflow/main.tf
@@ -21,6 +21,22 @@ resource "helm_release" "airflow" {
     repository = "https://airflow.apache.org/"
     chart = "airflow"
     version = "1.11.0"
-    
-    values = ["${data.template_file.airflow_values.rendered}"]
+
+    values = [
+      templatefile(
+        "modules/airflow/airflow_values.yaml",
+        {
+          registry-secret-name            = var.registry-secret.metadata[0].name
+          default-airflow-repository      = var.default-airflow-repository
+          airflow-image-tag               = var.airflow-image-tag
+          airflow-webserver-secret-result = random_password.airflow-webserver-secret.result
+          producer-image                  = var.producer-image
+          data-services-image-tag         = var.data-services-image-tag
+          namespace                       = kubernetes_namespace.airflow.metadata[0].name
+          producer-batch-config           = kubernetes_config_map.producer-config.metadata[0].name
+          gcp-creds-secret                = kubernetes_secret.gcp-creds.metadata[0].name
+          gcp-registry-secret             = var.registry-secret.metadata[0].name
+        }
+      )
+    ]
 }
